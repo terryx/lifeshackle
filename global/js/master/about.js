b4da15c7ae57306a0a659f19e7bf8f5fee3e1fca
Terry Yuen <terryxlife@gmail.com>
2011-10-23 10:07:48 +0800

    remove unused files

--- global/js/master/about.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-//	jQuery("#quote-form").validationEngine({
-//		ajaxFormValidation: true,
-//		onBeforeAjaxFormValidation: beforeCall,
-//		onAjaxFormComplete: ajaxCallback
-//	});
-
-});
-
--- protected/config/routes.conf.php
+++ protected/config/routes.conf.php
@@ -82,25 +82,25 @@ $route['*']['/test-page'] = array('TestController', 'testPage');
 
 //---------- Delete if not needed ------------
 
-$admin = array('admin'=>'1234');
-
-//view the logs and profiles XML, filename = db.profile, log, trace.log, profile
-$route['*']['/debug/:filename'] = array('MainController', 'debug', 'authName'=>'DooPHP Admin', 'auth'=>$admin, 'authFail'=>'Unauthorized!');
-
-//show all urls in app
-$route['*']['/allurl'] = array('MainController', 'allurl', 'authName'=>'DooPHP Admin', 'auth'=>$admin, 'authFail'=>'Unauthorized!');
-
-//generate routes file. This replace the current routes.conf.php. Use with the sitemap tool.
-$route['post']['/gen_sitemap'] = array('MainController', 'gen_sitemap', 'authName'=>'DooPHP Admin', 'auth'=>$admin, 'authFail'=>'Unauthorized!');
-
-//generate routes & controllers. Use with the sitemap tool.
-$route['post']['/gen_sitemap_controller'] = array('MainController', 'gen_sitemap_controller', 'authName'=>'DooPHP Admin', 'auth'=>$admin, 'authFail'=>'Unauthorized!');
-
-//generate Controllers automatically
-$route['*']['/gen_site'] = array('MainController', 'gen_site', 'authName'=>'DooPHP Admin', 'auth'=>$admin, 'authFail'=>'Unauthorized!');
-
-//generate Models automatically
-$route['*']['/gen_model'] = array('MainController', 'gen_model', 'authName'=>'DooPHP Admin', 'auth'=>$admin, 'authFail'=>'Unauthorized!');
-
+//$admin = array('admin'=>'1234');
+//
+////view the logs and profiles XML, filename = db.profile, log, trace.log, profile
+//$route['*']['/debug/:filename'] = array('MainController', 'debug', 'authName'=>'DooPHP Admin', 'auth'=>$admin, 'authFail'=>'Unauthorized!');
+//
+////show all urls in app
+//$route['*']['/allurl'] = array('MainController', 'allurl', 'authName'=>'DooPHP Admin', 'auth'=>$admin, 'authFail'=>'Unauthorized!');
+//
+////generate routes file. This replace the current routes.conf.php. Use with the sitemap tool.
+//$route['post']['/gen_sitemap'] = array('MainController', 'gen_sitemap', 'authName'=>'DooPHP Admin', 'auth'=>$admin, 'authFail'=>'Unauthorized!');
+//
+////generate routes & controllers. Use with the sitemap tool.
+//$route['post']['/gen_sitemap_controller'] = array('MainController', 'gen_sitemap_controller', 'authName'=>'DooPHP Admin', 'auth'=>$admin, 'authFail'=>'Unauthorized!');
+//
+////generate Controllers automatically
+//$route['*']['/gen_site'] = array('MainController', 'gen_site', 'authName'=>'DooPHP Admin', 'auth'=>$admin, 'authFail'=>'Unauthorized!');
+//
+////generate Models automatically
+//$route['*']['/gen_model'] = array('MainController', 'gen_model', 'authName'=>'DooPHP Admin', 'auth'=>$admin, 'authFail'=>'Unauthorized!');
+//
 
 ?>
